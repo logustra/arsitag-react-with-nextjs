449b932089bec0b191b0e30f19fa97cc947b4388
logustra <logustra@gmail.com>
2018-05-24 02:21:05 +0700

    update

--- components/Head/Head.jsx
+++ components/Head/Head.jsx
@@ -5,11 +5,12 @@ const defaultDescription = '';
 
 const Head = (props) => (
     <NextHead>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <title>{props.title || ''}</title>
         <meta name="description" content={props.description || defaultDescription} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/static/images/logo.png" type="image/png" />
+        <link rel="stylesheet" href="/static/css/bootstrap.min.css" />
     </NextHead>
 )
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,25 @@
-import Document, { Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
     static getInitialProps({ renderPage }) {
-    const sheet = new ServerStyleSheet()
-    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
-    const styleTags = sheet.getStyleElement()
-    return { ...page, styleTags }
-  }
+        const sheet = new ServerStyleSheet();
+        const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+        const styleTags = sheet.getStyleElement();
+        return { ...page, styleTags };
+    }
 
     render () {
-        const { pathname, query } = this.props;
-
         return (
-        <html>
-            <Head>
-            <link rel="stylesheet" href="/static/css/bootstrap.min.css" />
-            {this.props.styleTags}
-            </Head>
-            <body>
-            <Main />
-            <NextScript />
-            </body>
-        </html>
-    )
-  }
+            <html>
+                <Head>
+                    {this.props.styleTags}
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </html>
+        )
+    }
 }
